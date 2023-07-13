6b384135614f3fe83b27abb588eaaa53afa85565
Wifqo Arova Syams <56685562+ArovaSyams@users.noreply.github.com>
2023-07-13 21:47:14 +0300

    add a other user profile page and show only the other user prompt data

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,12 +5,19 @@ import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
-const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete, handleFeedProfile }) => {
   const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
 
   const [copied, setCopied] = useState("");
+
+  const handleUserProfile = () => {
+    
+    if(post.creator._id === session?.user.id) return router.push('/profile');
+
+    return router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  }
 
   const handleCopy = () => {
     setCopied(post.prompt);
@@ -21,7 +28,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
     <div className='prompt_card'>
       <div className='flex justify-between items-start gap-5'>
-        <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
+        <div onClick={handleUserProfile} className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
           <Image src={post.creator.image} alt='user_image' width={40} height={40} className='rounded-full object-contain' />
 
           <div className="flex flex-col">
@@ -31,7 +38,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         </div>
 
         <div className="copy-btn cursor-pointer" onClick={handleCopy}>
-          <Image src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'} width={12} height={12} />
+          <Image alt='copy' src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'} width={12} height={12} />
         </div>
       </div>
 
